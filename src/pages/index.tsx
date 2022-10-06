70e4c216ad237d6205d84c6485f3dd2b4f0e3896
Aimekram <janina.byszewska@gmail.com>
2022-10-06 21:31:47 +0200

    chore: cleanup unused components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { Box, Button, Container, Typography } from '@mui/material';
+import Link from '../components/Link';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -45,6 +47,26 @@ const Home: NextPage = () => {
             <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
+        <Container maxWidth='lg'>
+          <Box
+            sx={{
+              my: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Typography variant='h4' component='h1' gutterBottom>
+              MUI v5 + Next.js with TypeScript example
+            </Typography>
+            <Box maxWidth='sm'>
+              <Button variant='contained' component={Link} noLinkStyle href='/'>
+                Go to the home page
+              </Button>
+            </Box>
+          </Box>
+        </Container>
       </main>
 
       <footer className={styles.footer}>
